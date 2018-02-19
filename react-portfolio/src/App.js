d1fcd912155ac04aa7fae4c3828930cabfddd5be
dan <danbrianlewis@gmail.com>
2018-02-19 12:43:25 -0500

    initial commit

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Example from './components/wrapper'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Example />
+      </div>
+    );
+  }
+}
+
+export default App;
